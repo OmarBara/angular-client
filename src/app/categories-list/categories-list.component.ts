ead5eaafde213eec7a4cbe58ca5f14a12e789d54
Omar Bara <omarbara@ymail.com>
2020-01-16 20:08:51 +0100

    get list of categories

--- src/app/categories-list/categories-list.component.ts
+++ src/app/categories-list/categories-list.component.ts
@@ -6,15 +6,25 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './categories-list.component.html',
   styleUrls: ['./categories-list.component.scss']
 })
+
+
 export class CategoriesListComponent implements OnInit {
 catlist = [
  {name:"shawerma", img:'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
  { name:"burger", img:'https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg'}
 ]
-  
 constructor(private http : HttpClient) {}
+test : any[] = [] 
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.test 
+    var that = this
+    this.onGet(function(res){
+       that.catlist = res
+      // this.test = res
+      console.log( that.catlist)
+    })
+  }
 
   onPost(){
     this.http.post('http://localhost:8000/api/category/',{
@@ -24,10 +34,11 @@ constructor(private http : HttpClient) {}
       console.log(res)
       )
     }
-    onGet(){
+    onGet(cb){
       this.http.get('http://localhost:8000/api/category/')
       .subscribe( res =>
-        console.log(res)
+        cb(res)
+        
         )
     }
 
